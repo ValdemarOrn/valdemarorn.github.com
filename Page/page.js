6d1d3197f0b3771802ddf11791d1efac92ba995e
unknown <valdemaroe@LAP-IST000027.isbank.is>
2012-04-18 15:12:55 +0000

    sdf

--- Page/page.js
+++ Page/page.js
@@ -1,7 +1,12 @@
-alert('ping');
-var text = $('#content').html();
 
-var converter = new Showdown.converter();
-var html = converter.makeHtml(text);
+$(document).ready(function(){
 
-alert(html);
+	alert('ping');
+	var text = $('#content').html();
+
+	var converter = new Showdown.converter();
+	var html = converter.makeHtml(text);
+
+	alert(html);
+	
+});
